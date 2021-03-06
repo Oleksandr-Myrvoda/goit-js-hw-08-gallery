e8d47ecaa768f1a5fd7c00bb52023b4623385deb
Oleksandr Myrvoda <mirvoda2@gmail.com>
2021-03-06 10:34:13 +0200

    homework js 08 fix3

--- task-07.js
+++
@@ -1,9 +0,0 @@
-const sizeRef = document.querySelector('#font-size-control');
-console.log(sizeRef)
-const textRef = document.querySelector('#text');
-console.log(textRef)
-
-sizeRef.addEventListener('input', (e) => {
-    console.log(e)
-    textRef.style.fontSize = e.target.value+'px';
-})
